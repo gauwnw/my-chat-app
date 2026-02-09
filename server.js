62bbdcda0efc84e0a8c0c1a6965a335b3045bd36
gauwnw <72285907+gauwnw@users.noreply.github.com>
2026-02-09 23:48:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,18 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('유저 접속됨');
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
 
-// Render 배포를 위해 포트 설정 변경
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`서버가 ${PORT}에서 돌아가는 중!`);
+  console.log(`Server is running on port ${PORT}`);
 });
